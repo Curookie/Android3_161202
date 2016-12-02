6b7c63819739291cab2756524cfec8f68dffee49
Curookie <curookie@naver.com>
2016-12-02 14:28:45 +0900

    1. 레이아웃 생성

--- app/src/main/java/com/example/curookie/project6_1/MainActivity.java
+++ app/src/main/java/com/example/curookie/project6_1/MainActivity.java
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("시간 예약");
     }
 }
