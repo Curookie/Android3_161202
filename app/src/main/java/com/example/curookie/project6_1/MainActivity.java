3f211335c5eef5c311d0dc5d07bfad2ef2c96453
Curookie <curookie@naver.com>
2016-12-02 14:54:54 +0900

    2. [예약시작] 버튼을 클릭하면 그 위의 Chronometer가 작동한다.

--- app/src/main/java/com/example/curookie/project6_1/MainActivity.java
+++ app/src/main/java/com/example/curookie/project6_1/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.curookie.project6_1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.Chronometer;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn1;
+    Chronometer cro;
+    CalendarView cv;
+    TimePicker tp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("시간 예약");
+
+        btn1 = (Button)findViewById(R.id.button);
+        cro = (Chronometer)findViewById(R.id.chronometer);
+        cv = (CalendarView)findViewById(R.id.calendarView);
+        tp = (TimePicker)findViewById(R.id.timePicker);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cro.start();
+            }
+        });
     }
 }
